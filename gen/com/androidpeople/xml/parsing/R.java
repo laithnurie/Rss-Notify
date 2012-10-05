cf9a0e7bce695339de57124d20fa9a78ce75e13d
Laith Nurie <laithnurie@gmail.com>
2012-10-05 13:10:12 +0100

    Added edit text and button to fetch feed from the url

--- gen/com/androidpeople/xml/parsing/R.java
+++ gen/com/androidpeople/xml/parsing/R.java
@@ -14,8 +14,10 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int insideLayout=0x7f050001;
-        public static final int scrollView1=0x7f050000;
+        public static final int button1=0x7f050001;
+        public static final int editText1=0x7f050000;
+        public static final int insideLayout=0x7f050003;
+        public static final int scrollView1=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/androidpeople/xml/parsing/XMLParsingDOMExample.java
+++ src/com/androidpeople/xml/parsing/XMLParsingDOMExample.java
@@ -13,6 +13,9 @@ import org.xml.sax.InputSource;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,7 +24,8 @@ public class XMLParsingDOMExample extends Activity {
 	LinearLayout layout;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		/** Create a new layout to display the view */
@@ -29,73 +33,90 @@ public class XMLParsingDOMExample extends Activity {
 		
 		layout.setOrientation(1);
 		/** Create a new textview array to display the results */
-		TextView title[];
-		TextView link[];
-		TextView desc[];
-
-		try {
-
-			URL url = new URL("http://www.mangahere.com/rss/naruto.xml");
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(new InputSource(url.openStream()));
-			doc.getDocumentElement().normalize();
-
-			NodeList nodeList = doc.getElementsByTagName("item");
-
-			title = new TextView[10];
-			link = new TextView[10];
-			desc = new TextView[10];
-			
-			for (int i = 0; i < 10; i++) {
-
-				Node node = nodeList.item(i);
-
-				title[i] = new TextView(this);
-				link[i] = new TextView(this);
-				desc[i] = new TextView(this);
-
-				Element fstElmnt = (Element) node;
-				NodeList titleList = fstElmnt.getElementsByTagName("title");
-				Element titleElement = (Element) titleList.item(0);
-				titleList = titleElement.getChildNodes();
-				if (titleList.item(0) !=null) {
-					title[i].setText("Chapter No = "+ ((Node) titleList.item(0)).getNodeValue());
-				}
-				else {
-					title[i].setText("There is no Title");
-				}
-				
-
-				NodeList linkList = fstElmnt.getElementsByTagName("link");
-				Element linkElement = (Element) linkList.item(0);
-				linkList = linkElement.getChildNodes();
-				if (linkList.item(0) !=null) {
-					link[i].setText("Link = "+ ((Node) linkList.item(0)).getNodeValue());
-				}
-				else {
-					link[i].setText("no link available");
-				}
-
-				
-				NodeList descList = fstElmnt.getElementsByTagName("description");
-				Element descElement = (Element) descList.item(0);
-				descList = descElement.getChildNodes();
-				if (descList.item(0) !=null) {
-					desc[i].setText("title = "+ ((Node) descList.item(0)).getNodeValue());
-				}
-				else {
-					desc[i].setText("No Description available");
-				}
-				
-				layout.addView(title[i]);
-				layout.addView(link[i]);
-				layout.addView(desc[i]);
-			}
-		} 
 		
-		catch (Exception e) {
-			System.out.println("XML Pasing Excpetion = " + e);
-		}
+		Button button1 = (Button) findViewById(R.id.button1);
+		final EditText et = (EditText) findViewById(R.id.editText1);
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            getFeed(et.getText().toString());
+            
+            }
+        });
 	}
-}
+	
+	
+	public void getFeed(String feedUrl)
+	{
+	
+	TextView title[];
+	TextView link[];
+	TextView desc[];
+
+	try {
+
+		URL url = new URL(feedUrl);
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(new InputSource(url.openStream()));
+		doc.getDocumentElement().normalize();
+
+		NodeList nodeList = doc.getElementsByTagName("item");
+
+		title = new TextView[nodeList.getLength()];
+		link = new TextView[nodeList.getLength()];
+		desc = new TextView[nodeList.getLength()];
+		
+		for (int i = 0; i < nodeList.getLength(); i++) {
+
+			Node node = nodeList.item(i);
+
+			title[i] = new TextView(this);
+			link[i] = new TextView(this);
+			desc[i] = new TextView(this);
+
+			Element fstElmnt = (Element) node;
+			NodeList titleList = fstElmnt.getElementsByTagName("title");
+			Element titleElement = (Element) titleList.item(0);
+			titleList = titleElement.getChildNodes();
+			if (titleList.item(0) !=null) {
+				title[i].setText("Chapter No = "+ ((Node) titleList.item(0)).getNodeValue());
+			}
+			else {
+				title[i].setText("There is no Title");
+			}
+			
+
+			NodeList linkList = fstElmnt.getElementsByTagName("link");
+			Element linkElement = (Element) linkList.item(0);
+			linkList = linkElement.getChildNodes();
+			if (linkList.item(0) !=null) {
+				link[i].setText("Link = "+ ((Node) linkList.item(0)).getNodeValue());
+			}
+			else {
+				link[i].setText("no link available");
+			}
+
+			
+			NodeList descList = fstElmnt.getElementsByTagName("description");
+			Element descElement = (Element) descList.item(0);
+			descList = descElement.getChildNodes();
+			if (descList.item(0) !=null) {
+				desc[i].setText("title = "+ ((Node) descList.item(0)).getNodeValue());
+			}
+			else {
+				desc[i].setText("No Description available");
+			}
+			
+			layout.addView(title[i]);
+			layout.addView(link[i]);
+			layout.addView(desc[i]);
+		}
+	} 
+	
+	catch (Exception e) {
+		System.out.println("XML Pasing Excpetion = " + e);
+	}
+
+	}}
