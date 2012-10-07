45fec6263c363f5c0a364fe9314941bbcfe7a855
Laith Nurie <laithnurie@altair.config>
2012-10-07 22:06:05 +0100

    adding the database

---
+++ src/com/androidpeople/xml/parsing/Manga.java
@@ -0,0 +1,40 @@
+package com.androidpeople.xml.parsing;
+
+public class Manga {
+	
+		private long id;
+		private String manga;
+		private String chapter;
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getManga() {
+			return manga;
+		}
+
+		public void setManga(String manga) {
+			this.manga = manga;
+		}
+		
+		public String getChapter() {
+			return chapter;
+		}
+		
+		public void setChapter(String chapter) {
+			this.chapter = chapter;
+		}
+
+		// Will be used by the ArrayAdapter in the ListView
+		@Override
+		public String toString() {
+			return manga;
+		}
+		
+		
+}
