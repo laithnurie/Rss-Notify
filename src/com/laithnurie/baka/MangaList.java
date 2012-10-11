19089c3d648bda7076382ed483ebbefc72b597b5
Laith Nurie <laithnurie@altair.config>
2012-10-11 23:56:01 +0100

    Added asyncTask class to MangaList

--- src/com/laithnurie/baka/MangaList.java
+++ src/com/laithnurie/baka/MangaList.java
@@ -11,25 +11,38 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class MangaList extends Activity {
 	
-	LinearLayout mangaList;
+	private LinearLayout mangaList;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manga_list);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+                
+        Bundle extras = getIntent().getExtras();
+        String feedURL = extras.getString("feedUrl");
+       // getFeed(feedURL);
+        
+        new loadMangaList().execute(feedURL);
+        
+        
     }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,81 +62,122 @@ public class MangaList extends Activity {
     }
     
     
-    public void getFeed(String feedUrl)	{
-	
-	TextView title[];	
-	TextView link[];
-	TextView desc[];
+    
+    public class loadMangaList extends AsyncTask<String, Integer, TextView> {
+    	
+    	ProgressDialog pd;
+    	String passedParam;
 
-	mangaList.removeAllViewsInLayout();
 
-	try {
+    	@Override
+    	protected void onPreExecute() {
+    		
+    		pd = new ProgressDialog(MangaList.this);
+    		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+    		pd.setMax(100);
+    		pd.show();
+    		
+    	}
+    	
+    	@Override
+    	protected TextView doInBackground(final String... params) {
+    		
+    		TextView title[] =null;	
+    		TextView link[];
+    		TextView desc[];
+            mangaList = (LinearLayout) findViewById(R.id.mangaList);
+                	    		
+    		passedParam = params[0];
+    	
 
-		URL url = new URL("http://www.mangahere.com/rss/"+feedUrl+".xml");
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document doc = db.parse(new InputSource(url.openStream()));
-		doc.getDocumentElement().normalize();
+    	
+    		try {
+    	
+    			URL url = new URL("http://www.mangahere.com/rss/"+params[0]+".xml");
+    			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    			DocumentBuilder db = dbf.newDocumentBuilder();
+    			Document doc = db.parse(new InputSource(url.openStream()));
+    			doc.getDocumentElement().normalize();
+    	
+    			NodeList nodeList = doc.getElementsByTagName("item");
+    	
+    			title = new TextView[nodeList.getLength()];
+    			link = new TextView[nodeList.getLength()];
+    			desc = new TextView[nodeList.getLength()];
+    			
+	    		//mangaList.removeAllViewsInLayout();
 
-		NodeList nodeList = doc.getElementsByTagName("item");
+    			
+    			for (int i = 0; i < 100; i++) {
+    					
+    				Node node = nodeList.item(i);
+    	
+    				title[i] = new TextView(getApplicationContext());
+    				link[i] = new TextView(getApplicationContext());
+    				desc[i] = new TextView(getApplicationContext());
+    	
+    				Element fstElmnt = (Element) node;
+    				NodeList titleList = fstElmnt.getElementsByTagName("title");
+    				Element titleElement = (Element) titleList.item(0);
+    				titleList = titleElement.getChildNodes();
+    				if (titleList.item(0) !=null) {
+    					title[i].setText("Chapter No = "+ ((Node) titleList.item(0)).getNodeValue());
+    					//mangaList.addView(title[i]);
+    					
+    					return title[i];
+    	
+    				}
+    				else {
+    					title[i].setText("There is no Title");
+    				}	
+    				
+    	
+    				NodeList linkList = fstElmnt.getElementsByTagName("link");
+    				Element linkElement = (Element) linkList.item(0);
+    				linkList = linkElement.getChildNodes();
+    				if (linkList.item(0) !=null) {
+    					link[i].setText("Link = "+ ((Node) linkList.item(0)).getNodeValue());
+    					//mangaList.addView(link[i]);
+    	
+    				}
+    				else {
+    					link[i].setText("no link available");
+    				}
+    	
+    				
+    				NodeList descList = fstElmnt.getElementsByTagName("description");
+    				Element descElement = (Element) descList.item(0);
+    				descList = descElement.getChildNodes();
+    				if (descList.item(0) !=null) {
+    					desc[i].setText("title = "+ ((Node) descList.item(0)).getNodeValue());
+    					//mangaList.addView(desc[i]);
+    				}
+    				else {
+    					desc[i].setText("No Description available");
+    				}
+    				
+    				publishProgress(i);
+    				Thread.sleep(88);	
+    			}
+    		} 
+    		
+    		catch (Exception e) {
+    			Log.v("mangaList", e.getMessage());
+    		}
+    		return title[0];
+    	}
+    	
+    	@Override
+    	protected void onProgressUpdate(Integer... progress) {
+    		pd.incrementProgressBy(progress[0]);	
+    	}
+    	
+    	@Override
+        protected void onPostExecute(TextView result) {
+        	pd.dismiss();
 
-		title = new TextView[nodeList.getLength()];
-		link = new TextView[nodeList.getLength()];
-		desc = new TextView[nodeList.getLength()];
-		
-		for (int i = 0; i < 1; i++) {
-
-			Node node = nodeList.item(i);
-
-			title[i] = new TextView(this);
-			link[i] = new TextView(this);
-			desc[i] = new TextView(this);
-
-			Element fstElmnt = (Element) node;
-			NodeList titleList = fstElmnt.getElementsByTagName("title");
-			Element titleElement = (Element) titleList.item(0);
-			titleList = titleElement.getChildNodes();
-			if (titleList.item(0) !=null) {
-				title[i].setText("Chapter No = "+ ((Node) titleList.item(0)).getNodeValue());
-				mangaList.addView(title[i]);
-
-			}
-			else {
-				title[i].setText("There is no Title");
-			}	
-			
-
-			NodeList linkList = fstElmnt.getElementsByTagName("link");
-			Element linkElement = (Element) linkList.item(0);
-			linkList = linkElement.getChildNodes();
-			if (linkList.item(0) !=null) {
-				link[i].setText("Link = "+ ((Node) linkList.item(0)).getNodeValue());
-				mangaList.addView(link[i]);
-
-			}
-			else {
-				link[i].setText("no link available");
-			}
-
-			
-			NodeList descList = fstElmnt.getElementsByTagName("description");
-			Element descElement = (Element) descList.item(0);
-			descList = descElement.getChildNodes();
-			if (descList.item(0) !=null) {
-				desc[i].setText("title = "+ ((Node) descList.item(0)).getNodeValue());
-				mangaList.addView(desc[i]);
-			}
-			else {
-				desc[i].setText("No Description available");
-			}
-			
-		}
-	} 
-	
-	catch (Exception e) {
-		System.out.println("XML Pasing Excpetion = " + e);
-	}
-
-	}
-
+    		Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_LONG).show();
+    		mangaList.addView(result);
+        }
+    }
 }
--- src/com/laithnurie/baka/MangaMenu.java
+++ src/com/laithnurie/baka/MangaMenu.java
@@ -1,7 +1,10 @@
 package com.laithnurie.baka;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MangaMenu extends Activity {
 	
@@ -9,5 +12,29 @@ public class MangaMenu extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		Button narutoBtn = (Button) findViewById(R.id.narutoBtn);
+		
+		narutoBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	mangaIntentSwitcher("naruto");
+            }
+        });
+		
+		
+		Button bleachBtn = (Button) findViewById(R.id.bleachBtn);
+		
+		bleachBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	mangaIntentSwitcher("bleach");
+            }
+        });
+	}
+	
+	public void mangaIntentSwitcher(String manga)
+	{
+		Intent i = new Intent(getApplicationContext(), MangaList.class);
+    	i.putExtra("feedUrl", manga);
+    	startActivity(i);
 	}
 }
