9ba81b00f2214c9fa8ebd8f6a3b41496e586d471
Laith Nurie <laithnurie@gmail.com>
2012-10-22 12:58:40 +0100

    Dashboard feature tested but not implemented fully

---
+++ src/com/laithnurie/baka/DashboardActivity.java
@@ -0,0 +1,60 @@
+package com.laithnurie.baka;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class DashboardActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        
+        //attach event handler to dash buttons
+        DashboardClickListener dBClickListener = new DashboardClickListener();
+        findViewById(R.id.dashboard_button_add).setOnClickListener(dBClickListener);
+        findViewById(R.id.dashboard_button_viewall).setOnClickListener(dBClickListener);
+        findViewById(R.id.dashboard_button_manage).setOnClickListener(dBClickListener);
+        findViewById(R.id.dashboard_button_personalbests).setOnClickListener(dBClickListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_dashboard, menu);
+        return true;
+    }
+    
+    private class DashboardClickListener implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent i = null;
+            switch (v.getId()) {
+                case R.id.dashboard_button_add:
+                    i = new Intent(DashboardActivity.this, MainActivity.class);
+                    break;
+                case R.id.dashboard_button_viewall:
+                    i = new Intent(DashboardActivity.this, MainActivity.class);
+                    break;
+                case R.id.dashboard_button_manage:
+                    i = new Intent(DashboardActivity.this, MainActivity.class);
+                    break;
+                case R.id.dashboard_button_personalbests:
+                    i = new Intent(DashboardActivity.this, MainActivity.class);
+                    break;
+                default:
+                    break;
+            }
+            if(i != null) {
+                startActivity(i);
+            }
+        }
+    }
+    
+    
+}
+
+
