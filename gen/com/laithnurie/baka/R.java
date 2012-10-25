3673bc55218709f98652f05307268c6dcf590f4f
Laith Nurie <laithnurie@gmail.com>
2012-10-25 13:15:15 +0100

    added spinners to national rail and populated with values from string array station_name under strings.xml

--- gen/com/laithnurie/baka/R.java
+++ gen/com/laithnurie/baka/R.java
@@ -8,23 +8,26 @@
 package com.laithnurie.baka;
 
 public final class R {
+    public static final class array {
+        public static final int station_name=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int bakerloo=0x7f060000;
-        public static final int central=0x7f060001;
-        public static final int circle=0x7f060002;
-        public static final int district=0x7f060003;
-        public static final int dlr=0x7f060004;
-        public static final int hammersmith=0x7f060005;
-        public static final int jubilee=0x7f060006;
-        public static final int metropolitan=0x7f060007;
-        public static final int northern=0x7f060008;
-        public static final int overground=0x7f060009;
-        public static final int piccadily=0x7f06000a;
-        public static final int status=0x7f06000d;
-        public static final int victoria=0x7f06000b;
-        public static final int waterloo=0x7f06000c;
+        public static final int bakerloo=0x7f070000;
+        public static final int central=0x7f070001;
+        public static final int circle=0x7f070002;
+        public static final int district=0x7f070003;
+        public static final int dlr=0x7f070004;
+        public static final int hammersmith=0x7f070005;
+        public static final int jubilee=0x7f070006;
+        public static final int metropolitan=0x7f070007;
+        public static final int northern=0x7f070008;
+        public static final int overground=0x7f070009;
+        public static final int piccadily=0x7f07000a;
+        public static final int status=0x7f07000d;
+        public static final int victoria=0x7f07000b;
+        public static final int waterloo=0x7f07000c;
     }
     public static final class drawable {
         public static final int bleach=0x7f020000;
@@ -42,49 +45,53 @@ public final class R {
         public static final int tube=0x7f02000c;
     }
     public static final class id {
-        public static final int baker_stat=0x7f08000a;
-        public static final int bakerloo=0x7f080009;
-        public static final int bleachBtn=0x7f080028;
-        public static final int central=0x7f08000b;
-        public static final int central_stat=0x7f08000c;
-        public static final int chapter=0x7f080023;
-        public static final int circle=0x7f08000d;
-        public static final int circle_stat=0x7f08000e;
-        public static final int desc=0x7f080025;
-        public static final int district=0x7f08000f;
-        public static final int district_stat=0x7f080010;
-        public static final int dlr=0x7f080011;
-        public static final int dlr_stat=0x7f080012;
-        public static final int hammer_stat=0x7f080014;
-        public static final int hammersmith=0x7f080013;
-        public static final int jubilee=0x7f080015;
-        public static final int jubilee_stat=0x7f080016;
-        public static final int linearlayout1=0x7f080008;
-        public static final int manga=0x7f080024;
-        public static final int mangaList=0x7f080002;
-        public static final int mangaSection=0x7f080000;
-        public static final int menu_settings=0x7f08002a;
-        public static final int metro=0x7f080017;
-        public static final int metro_stat=0x7f080018;
-        public static final int narutoBtn=0x7f080027;
-        public static final int national=0x7f080006;
-        public static final int north_stat=0x7f08001a;
-        public static final int northern=0x7f080019;
-        public static final int onepieceBtn=0x7f080029;
-        public static final int over_stat=0x7f08001c;
-        public static final int overground=0x7f08001b;
-        public static final int piccadily=0x7f08001d;
-        public static final int piccadily_stat=0x7f08001e;
-        public static final int pubDate=0x7f080026;
-        public static final int relLayout1=0x7f080007;
-        public static final int srListView=0x7f080003;
-        public static final int travel=0x7f080001;
-        public static final int tube=0x7f080005;
-        public static final int victoria=0x7f08001f;
-        public static final int victoria_stat=0x7f080020;
-        public static final int water_stat=0x7f080022;
-        public static final int waterloo=0x7f080021;
-        public static final int webView1=0x7f080004;
+        public static final int TextView01=0x7f090008;
+        public static final int baker_stat=0x7f09000e;
+        public static final int bakerloo=0x7f09000d;
+        public static final int bleachBtn=0x7f09002c;
+        public static final int central=0x7f09000f;
+        public static final int central_stat=0x7f090010;
+        public static final int chapter=0x7f090027;
+        public static final int circle=0x7f090011;
+        public static final int circle_stat=0x7f090012;
+        public static final int desc=0x7f090029;
+        public static final int district=0x7f090013;
+        public static final int district_stat=0x7f090014;
+        public static final int dlr=0x7f090015;
+        public static final int dlr_stat=0x7f090016;
+        public static final int from_spinner=0x7f090007;
+        public static final int hammer_stat=0x7f090018;
+        public static final int hammersmith=0x7f090017;
+        public static final int jubilee=0x7f090019;
+        public static final int jubilee_stat=0x7f09001a;
+        public static final int linearlayout1=0x7f090005;
+        public static final int manga=0x7f090028;
+        public static final int mangaList=0x7f090002;
+        public static final int mangaSection=0x7f090000;
+        public static final int menu_settings=0x7f09002e;
+        public static final int metro=0x7f09001b;
+        public static final int metro_stat=0x7f09001c;
+        public static final int narutoBtn=0x7f09002b;
+        public static final int national=0x7f09000b;
+        public static final int north_stat=0x7f09001e;
+        public static final int northern=0x7f09001d;
+        public static final int onepieceBtn=0x7f09002d;
+        public static final int over_stat=0x7f090020;
+        public static final int overground=0x7f09001f;
+        public static final int piccadily=0x7f090021;
+        public static final int piccadily_stat=0x7f090022;
+        public static final int pubDate=0x7f09002a;
+        public static final int relLayout1=0x7f09000c;
+        public static final int srListView=0x7f090003;
+        public static final int textView1=0x7f090006;
+        public static final int to_spinner=0x7f090009;
+        public static final int travel=0x7f090001;
+        public static final int tube=0x7f09000a;
+        public static final int victoria=0x7f090023;
+        public static final int victoria_stat=0x7f090024;
+        public static final int water_stat=0x7f090026;
+        public static final int waterloo=0x7f090025;
+        public static final int webView1=0x7f090004;
     }
     public static final class layout {
         public static final int activity_dashboard=0x7f030000;
@@ -98,13 +105,13 @@ public final class R {
         public static final int main=0x7f030008;
     }
     public static final class menu {
-        public static final int activity_dashboard=0x7f070000;
-        public static final int activity_main=0x7f070001;
-        public static final int activity_manga_list=0x7f070002;
-        public static final int activity_manga_viewer=0x7f070003;
-        public static final int activity_national_rail=0x7f070004;
-        public static final int activity_travel=0x7f070005;
-        public static final int activity_tube=0x7f070006;
+        public static final int activity_dashboard=0x7f080000;
+        public static final int activity_main=0x7f080001;
+        public static final int activity_manga_list=0x7f080002;
+        public static final int activity_manga_viewer=0x7f080003;
+        public static final int activity_national_rail=0x7f080004;
+        public static final int activity_travel=0x7f080005;
+        public static final int activity_tube=0x7f080006;
     }
     public static final class string {
         public static final int app_name=0x7f040002;
@@ -139,6 +146,6 @@ public final class R {
         public static final int waterloo=0x7f04001c;
     }
     public static final class style {
-        public static final int DashboardButton=0x7f050000;
+        public static final int DashboardButton=0x7f060000;
     }
 }
--- src/com/laithnurie/baka/NationalRail.java
+++ src/com/laithnurie/baka/NationalRail.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.support.v4.app.NavUtils;
 
 public class NationalRail extends Activity {
@@ -12,6 +14,15 @@ public class NationalRail extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_national_rail);
+        
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        		  this, R.array.station_name, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner from = (Spinner) findViewById( R.id.from_spinner );
+        Spinner to = (Spinner) findViewById( R.id.to_spinner );
+        
+        from.setAdapter(adapter);
+        to.setAdapter(adapter);
     }
 
     @Override
