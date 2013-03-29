d41edc00177679d627042a7eca44eb35106922d2
Laith Nurie <laithnurie@gmail.com>
2013-03-29 13:05:52 +0000

    fixed project

--- src/com/laithnurie/baka/NationalTrains.java
+++ src/com/laithnurie/baka/NationalTrains.java
@@ -43,7 +43,7 @@ public class NationalTrains extends Activity {
         fromStation = extras.getString("from");
         toStation = extras.getString("to");
 
-        trainUrlDate = "http://ojp.nationalrail.co.uk/service/ldb/liveTrainsJson?departing=true&liveTrainsFrom="+fromStation+"&liveTrainsTo=&serviceId=";
+        trainUrlDate = "http://ojp.nationalrail.co.uk/service/ldb/liveTrainsJson?departing=true&liveTrainsFrom="+fromStation+"&liveTrainsTo="+toStation+"&serviceId=";
         new loadTubeJson().execute();
     }
 
